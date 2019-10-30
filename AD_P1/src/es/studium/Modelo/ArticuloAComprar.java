0dbe79ba7dfe654748a0bafa90afa3b301cc302c
José Manuel Platero <48911682+V13Evrael@users.noreply.github.com>
2019-10-30 14:31:37 +0100

    Initial commit

---
+++ AD_P1/src/es/studium/Modelo/ArticuloAComprar.java
@@ -0,0 +1,48 @@
+package es.studium.Modelo;
+
+public class ArticuloAComprar {
+
+	private String descripcion;
+	private String unidad;
+	private Integer cantidad;
+	
+	public ArticuloAComprar() {
+		
+		descripcion = "";
+		unidad = "";
+		cantidad = 0;
+	}
+	
+	public ArticuloAComprar(String desc, String uni, Integer cant) {
+		
+		descripcion = desc;
+		unidad = uni;
+		cantidad = cant;
+		
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getUnidad() {
+		return unidad;
+	}
+
+	public void setUnidad(String unidad) {
+		this.unidad = unidad;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+}
---
+++ AD_P1/src/es/studium/Modelo/ListaDeLaCompra.java
@@ -0,0 +1,104 @@
+package es.studium.Modelo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class ListaDeLaCompra {
+
+	private String nombre;
+	private ArrayList<ArticuloAComprar> lista = new ArrayList<ArticuloAComprar>();
+
+	public ListaDeLaCompra() {
+
+		nombre = "";
+	}
+
+	public ListaDeLaCompra(String nombre) {
+
+		this.nombre = nombre;
+	}
+
+	public String getNombreLista() {
+
+		return this.nombre;
+	}
+
+	public void setNombreLista(String nuevoNombre) {
+
+		this.nombre = nuevoNombre;
+	}
+
+	public void agregarProductoAComprar(ArticuloAComprar articulo) {
+
+		this.lista.add(articulo);
+	}
+
+	public ArrayList<ArticuloAComprar> getArticulos() {
+
+		return this.lista;
+	}
+
+	public void eliminarArticulo(String desc) {
+
+		for (ArticuloAComprar art : this.getArticulos()) {
+			if (art.getDescripcion().equals(desc)) {
+				this.getArticulos().remove(art);
+				break;
+			}
+		}
+	}
+
+	public void exportaListaDeLaCompra() {
+
+		try {
+			
+			File listaDataExp = new File("./datosListas/" + this.getNombreLista() + ".data");
+			FileOutputStream fos = new FileOutputStream(listaDataExp);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.close();
+			
+			oos.writeObject(this);
+			
+		} catch (FileNotFoundException e) {
+
+			System.out.println("El archivo no se encuentra");
+			
+		} catch (IOException e) {
+			
+			System.out.println("Se ha producido un error en la entrada/salida de los datos");
+		}
+	}
+	
+	public static ListaDeLaCompra importaListaDeLaCompra(String rutaAbsoluta) {
+		
+		ListaDeLaCompra resultado = new ListaDeLaCompra();
+		
+		try {
+			File listaDataImp = new File(rutaAbsoluta);
+			FileInputStream fis = new FileInputStream(listaDataImp);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			resultado = (ListaDeLaCompra) ois.readObject();
+			ois.close();
+			
+		} catch (FileNotFoundException e) {
+		
+			System.out.println("El archivo no se encuentra");
+
+		} catch (IOException e) {
+			
+			System.out.println("Se ha producido un error en la entrada/salida de los datos");
+			
+		} catch (ClassNotFoundException e) {
+			
+			System.out.println("La clase del objeto leído no se corresponde con una Lista De La Compra");
+		} 
+		
+		return resultado;
+	}
+}
